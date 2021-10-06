b4a1491d90f094af346f44d3a87339e6eadcee4f
AndiPY33 <zw-a@gmx.de>
2021-10-07 01:04:01 +0200

    Day 33: Different Animation Styles

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,20 +7,48 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var isShowingOrange = false
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingOrange.toggle()
+                }
+            }
+            .foregroundColor(self.isShowingOrange ? .orange : .white)
+            if isShowingOrange {
+                ZStack {
+
+                    Rectangle()
+                        .fill(Color.orange)
+                        .frame(width: 200, height: 200)
+                        .transition(.pivot)
+                    Text("Hello World!")
+                        .foregroundColor(.black)
+                        .font(.title)
+                }
+                .transition(.pivot)
+
             }
         }
-        .padding(50)
-        .background(Color.orange)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 1))
     }
 }
 

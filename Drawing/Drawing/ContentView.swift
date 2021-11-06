5154ce15fe6f80b1fbe8894d2557fadb65cb7e71
AndiPY33 <zw-a@gmx.de>
2021-11-06 12:38:41 +0100

    Finished Day 46 + Spirograph of Day 45

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -40,7 +40,7 @@ struct ColorCyclingRectangle: View {
             ForEach(0..<steps) { value in
                 RoundedRectangle(cornerRadius: 20)
                     .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 10)
             }
         }
         .drawingGroup()
@@ -57,17 +57,101 @@ struct ColorCyclingRectangle: View {
     }
 }
 
+// Spirograph of Day 45
+struct Spirograph: Shape {
+    let innerRaidus: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRaidus, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRaidus)
+        let distance = CGFloat(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        
+        return path
+    }
+}
+
 
 struct ContentView: View {
     @State private var showArr = false
     @State private var moveArr = 0
-    @State private var amount: CGFloat = 10.0
+    @State private var slideVal: CGFloat = 10.0
     
     @State private var colorCycle = 0.0
+    
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6
     
     var body: some View {
         ScrollView {
             VStack {
+                Spacer()
+                
+                // Spirograph of Day 45
+                Spirograph(innerRaidus: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                    .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                    .frame(width: 300, height: 300)
+                
+                Group {
+                    Text("Inner radius: \(Int(innerRadius))")
+                    Slider(value: $innerRadius, in: 10...150, step: 1)
+                        .padding([.horizontal, .bottom])
+                    
+                    Text("Outer radius: \(Int(innerRadius))")
+                    Slider(value: $outerRadius, in: 10...150, step: 1)
+                        .padding([.horizontal, .bottom])
+                    
+                    Text("Distance: \(Int(distance))")
+                    Slider(value: $distance, in: 10...150, step: 1)
+                        .padding([.horizontal, .bottom])
+                    
+                    Text("Amount: \(Int(amount), specifier: "%.2f")")
+                    Slider(value: $amount)
+                        .padding([.horizontal, .bottom])
+                    
+                    Text("Color: \(Int(hue))")
+                    Slider(value: $hue)
+                        .padding(.horizontal)
+                }
+                
+                Spacer()
                 
                 ColorCyclingRectangle(amount: self.colorCycle)
                     .frame(width: 300, height: 300)
@@ -76,24 +160,22 @@ struct ContentView: View {
                     .padding()
                 
                 Arrow(arrWidthL: 50, arrWidthR: 90, arrHeadHight: 150)
-                    .stroke(Color.red, style: StrokeStyle(lineWidth: amount, lineCap: .round, lineJoin: .round))
+                    .stroke(Color.red, style: StrokeStyle(lineWidth: slideVal, lineCap: .round, lineJoin: .round))
                     .frame(width: 140, height: 400)
                     .opacity(self.showArr ? 0 : 1)
                     .offset(x: 0, y: self.showArr ? -700 : 0)
                     .animation(.easeInOut(duration: self.showArr ? 0.5 : 1))
                 
-                Slider(value: $amount, in: 5...30)
+                Slider(value: $slideVal, in: 5...30)
+                Text("Line width: \(Int(round(slideVal)))")
                 
-                Text("Line width: \(Int(round(amount)))")
-                
-                Button(self.showArr ? "Get Arrow back!" : "Shoot Arrow!") {
+                Button(action: {
                     self.showArr.toggle()
+                }) {
+                    Label(self.showArr ? "Get Arrow back!" : "Shoot Arrow!", systemImage: "arrow.up")
                 }
-                .padding()
+                
             }
-            .padding(120)
-            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 1, y: 0.25, width: 1, height: 1), scale: 0.15), width: 70)
-            .ignoresSafeArea(.all)
         }
     }
 }

74726d084100a81ff0de08ee3e30f7197966987e
AndiPY33 <zw-a@gmx.de>
2021-11-19 16:44:10 +0100

    Finished Day 49

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Andreas Zwikirsch on 18.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    
+    var body: some View {
+        Text("Day 50 of '100 Days of SwiftUI'")
+            .padding()
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Andreas Zwikirsch on 18.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

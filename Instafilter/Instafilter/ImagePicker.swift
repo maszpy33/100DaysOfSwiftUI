63b67ec55b63b21fed5ca88ceb720eb2b98988e8
JokerCode <77498718+maszpy33@users.noreply.github.com>
2022-04-28 14:15:20 +0200

    Day 64

--- Instafilter/Instafilter/ImagePicker.swift
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -10,15 +10,45 @@ import SwiftUI
 
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    // nested class, could also be initialized outside of the struct
+    class Coordinator: NSObject, PHPickerViewControllerDelegate  {
+        // parant is the binding image of the struct ImagePicker
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
+    
     func makeUIViewController(context: Context) -> some UIViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
         return picker
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        // struct ImagePicker passes itself to the instans of the class Coordinator
+        Coordinator(self)
     }
 }

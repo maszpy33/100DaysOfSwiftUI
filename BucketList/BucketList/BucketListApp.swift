e38878dabd8dea97800f73df9dd9c767e3751699
JokerCode <77498718+maszpy33@users.noreply.github.com>
2022-07-21 23:58:36 +0200

    Day 68

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Andreas Zwikirsch on 18.07.22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

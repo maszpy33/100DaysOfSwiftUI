be0227ddf5a8340233aafe1b4f1582c4eafe24f4
JokerCode <77498718+maszpy33@users.noreply.github.com>
2022-07-28 23:37:22 +0200

    Day 74

--- AccessibilityIntroduction/AccessibilityIntroduction/ContentView.swift
+++ AccessibilityIntroduction/AccessibilityIntroduction/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let pictures = [
+        "GOlang1",
+        "HolowKnightLogo",
+        "Joker_code_logo_darkBG",
+        "Le_Brain_logo",
+        "linux"
+    ]
+    
+    let labels = [
+        "Programming language",
+        "Game Character",
+        "Programmer Logo",
+        "Brain creative and analytic part illustraition",
+        "most used operating system logo"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    @State private var value = 10
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Value: \(value)")
+                .padding()
+            
+            Button("Increment") {
+                value += 1
+            }
             .padding()
+            
+            Button("Decrement") {
+                value -= 1
+            }
+            .padding()
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
     }
 }
 

963f6c96f31ca7106589bbeaae8569b5a5ad52bb
JokerCode <77498718+maszpy33@users.noreply.github.com>
2022-08-31 23:48:39 +0200

    Day 87

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.scenePhase) var scenePhase
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    @State private var currTime = 0
+    
     var body: some View {
         VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
+            Text("\(currTime)")
+            Text("Hello World!")
+                .onReceive(timer) { time in
+                    if counter == 100 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    
+                    counter += 1
+                }
         }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            print("VStack tapped!")
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                print("Active")
+            } else if newPhase == .inactive {
+                print("Inactive")
+            } else if newPhase == .background {
+                print("Background")
+            }
         }
     }
 }
